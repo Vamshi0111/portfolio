d373ede21166d4492c516bafb78d9bac42f3d35e
Vamshi0111 <vamshichary117@gmail.com>
2026-01-02 00:22:05 +0530

    Updates

--- _next/static/chunks/app/layout-f44eddf1e5c9a6c4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},2619:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,347,23)),Promise.resolve().then(l.t.bind(l,7311,23))},7311:e=>{e.exports={style:{fontFamily:"'Jost', 'Jost Fallback'",fontStyle:"normal"},className:"__className_74d74f"}}},e=>{var s=s=>e(e.s=s);e.O(0,[929,441,684,358],()=>s(2619)),_N_E=e.O()}]);
